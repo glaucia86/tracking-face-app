a9d84598533fab23bda5250064b94f21a5c8956f
Glaucia Lemos <glaucia_lemos86@hotmail.com>
2018-10-03 11:21:50 -0300

    Alteração no css do projeto.

--- src/js/app.js
+++ src/js/app.js
@@ -3,7 +3,7 @@
  * Data: 03/10/2018
  * Description: Arquivo responsável por tratar a parte da lógica do desenvolvimento
  * do reconhecimento facial por meio da lib: tracking.js
- * Author: Glaucia Lemos
+ * @author: Glaucia Lemos
  */
 
 'use strict'
