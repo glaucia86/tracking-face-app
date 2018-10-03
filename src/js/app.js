eab7b2aea1939d7930b00891aaaeb6492d72e92a
Glaucia Lemos <glaucia_lemos86@hotmail.com>
2018-10-03 01:12:12 -0300

    Inclusão de texto para testar a dimensão do canvas que faz o reconhecimento da face.

--- src/js/app.js
+++ src/js/app.js
@@ -16,7 +16,17 @@ function init() {
 
     tracking.track('#video', tracker, { camera: true });
     tracker.on('track', event => {
-        console.log(event);
+        // console.log(event);
+        context.clearRect(0,0, canvas.width, canvas.height);
+        event.data.forEach(rect => {
+            context.strokeStyle = '#ff0000';
+            context.lineWidth = 2;
+            context.strokeRect(rect.x, rect.y, rect.width, rect.height);
+
+            // Aqui estou testando preenchimento de texto de info do canvas que indentifica a face:
+            context.fillText(`x: ${rect.x}, w: ${rect.width}`, rect.x + rect.width + 20, rect.y + 20);
+            context.fillText(`y: ${rect.y}, h: ${rect.height}`, rect.x + rect.width + 20, rect.y + 40);
+        });
     });
 }
 window.onload = init();
