8f243ece324863434b536dec13798340b73049c9
Glaucia Lemos <glaucia_lemos86@hotmail.com>
2018-10-03 11:33:14 -0300

    Alteração nos contextos da aplicação.

--- src/js/app.js
+++ src/js/app.js
@@ -12,17 +12,21 @@ function init() {
     const video = document.getElementById('video');
     const canvas = document.getElementById('canvas');
     const context = canvas.getContext('2d');
+
     const tracker = new tracking.ObjectTracker('face');
 
     tracking.track('#video', tracker, { camera: true });
+
     tracker.on('track', event => {
         // console.log(event);
         context.clearRect(0,0, canvas.width, canvas.height);
         event.data.forEach(rect => {
             context.strokeStyle = '#ff0000';
-            context.lineWidth = 2;
             context.strokeRect(rect.x, rect.y, rect.width, rect.height);
-
+            context.font = '12px Helvetica';
+            context.fillStyle = "#fff";
+            context.lineWidth = 2;
+            
             // Aqui estou testando preenchimento de texto de info do canvas que indentifica a face:
             context.fillText(`x: ${rect.x}, w: ${rect.width}`, rect.x + rect.width + 20, rect.y + 20);
             context.fillText(`y: ${rect.y}, h: ${rect.height}`, rect.x + rect.width + 20, rect.y + 40);
