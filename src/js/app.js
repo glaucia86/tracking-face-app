1c1a5e08d4bb32d778fe5da0c3aeadc5b170563f
Glaucia Lemos <glaucia_lemos86@hotmail.com>
2018-10-02 23:52:39 -0300

    Criando o arquivo app.js para desenvolver a lógica da app.

---
+++ src/js/app.js
@@ -0,0 +1,8 @@
+/**
+ * File: app.js
+ * Data: 03/10/2018
+ * Description: Arquivo responsável por tratar a parte da lógica do desenvolvimento
+ * do reconhecimento facial por meio da lib: tracking.js
+ * Author: Glaucia Lemos
+ */
+
