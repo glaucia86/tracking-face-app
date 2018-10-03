8c9e2f563fa44e5bc790958847cecf107e61dd81
Glaucia Lemos <glaucia_lemos86@hotmail.com>
2018-10-03 00:09:29 -0300

    Continuação do desenvolvimento da app.

--- src/js/app.js
+++ src/js/app.js
@@ -6,3 +6,17 @@
  * Author: Glaucia Lemos
  */
 
+'use strict'
+
+function init() {
+    const video = document.getElementById('video');
+    const canvas = document.getElementById('canvas');
+    const context = canvas.getContext('2d');
+    const tracker = new tracking.ObjectTracker('face');
+
+    tracking.track('#video', tracker, { camera: true });
+    tracker.on('track', event => {
+        console.log(event);
+    });
+}
+window.onload = init();
